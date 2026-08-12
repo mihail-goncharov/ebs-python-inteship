7c6ee769e7dd00d9ff4387bb67d4731b389aae83
Goncharov Mihail <forprogramm11@gmail.com>
2026-08-12 15:07:03 +0300

    Add __str__ and verbose_name_plural to Category model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -7,6 +7,12 @@ class Category(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, db_index=True)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
 
 class Blog(models.Model):
     title = models.CharField(max_length=255, unique=True)
